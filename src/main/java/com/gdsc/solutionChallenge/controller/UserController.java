d8a4fef46b4985e0bdbf209fe557ca20b8034642
김산 <kimsan@gimsan-ui-MacBookAir.local>
2023-02-17 20:51:56 +0900

    first commit

---
+++ src/main/java/com/gdsc/solutionChallenge/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.gdsc.solutionChallenge.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/sign", produces = "application/json;charset=UTF-8")
+public class UserController {
+
+    @GetMapping
+    public String test() {
+        return "test";
+    }
+}
---
+++ src/test/java/com/gdsc/solutionChallenge/SolutionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.solutionChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
