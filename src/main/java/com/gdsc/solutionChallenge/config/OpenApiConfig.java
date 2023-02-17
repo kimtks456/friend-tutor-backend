406704967825a342d4cdcef42d4d52265d2d3da9
김산 <kimsan@gimsan-ui-MacBookAir.local>
2023-02-18 02:47:01 +0900

    Feat: Swagger api docs tool

---
+++ src/main/java/com/gdsc/solutionChallenge/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.gdsc.solutionChallenge.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI(@Value("v1.0") String appVersion) {
+
+        Info info = new Info().title("LMS API Docs").version(appVersion)
+                .description("2023 Solution Challenge API Docs")
+                .termsOfService("http://swagger.io/terms/")
+                .contact(new Contact().name("San Kim").url("https://github.com/kimtks456/").email("kimtks456@gmail.com"));
+//                .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
--- src/main/java/com/gdsc/solutionChallenge/controller/UserController.java
+++ src/main/java/com/gdsc/solutionChallenge/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/sign", produces = "application/json;charset=UTF-8")
+@RequestMapping(path = "/user", produces = "application/json;charset=UTF-8")
 public class UserController {
 
     @GetMapping
