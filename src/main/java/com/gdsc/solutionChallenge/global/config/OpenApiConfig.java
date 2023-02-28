ad9d667d7ec16c97be2b38cc4d06162bd8d3181d
김산 <kimsan@gimsan-ui-MacBookAir.local>
2023-02-28 16:42:44 +0900

    docs(config): enable swagger authorization header (close #6)

--- src/main/java/com/gdsc/solutionChallenge/global/config/OpenApiConfig.java
+++ src/main/java/com/gdsc/solutionChallenge/global/config/OpenApiConfig.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -17,10 +21,19 @@ public class OpenApiConfig {
         Info info = new Info().title("LMS API Docs").version(appVersion)
                 .description("2023 Solution Challenge API Docs")
                 .termsOfService("http://swagger.io/terms/")
-                .contact(new Contact().name("San Kim").url("https://github.com/kimtks456/").email("kimtks456@gmail.com"));
-//                .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
+                .contact(new Contact().name("San Kim").url("https://github.com/kimtks456/").email("kimtks456@gmail.com"))
+                .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(In.HEADER).name("Authorization");
+        SecurityRequirement schemaRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(schemaRequirement))
                 .info(info);
     }
 }
